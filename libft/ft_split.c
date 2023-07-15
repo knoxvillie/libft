023c439a7f40755f59c4d76696abb7cdf0bfc7a7
Kelvin Faustino <kelvinfaustino@gmail.com>
2023-07-15 16:38:24 +0100

    update libft

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: kfaustin <kfaustin@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 13:20:00 by kfaustin          #+#    #+#             */
-/*   Updated: 2023/03/01 12:18:16 by kfaustin         ###   ########.fr       */
+/*   Updated: 2023/05/27 16:36:33 by kfaustin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,16 +32,16 @@ static int	ft_wordcount(char const *s, char c)
 
 	i = 0;
 	count = 0;
-	while (*(s + i) != '\0')
+	while (s[i])
 	{
-		while (*(s + i) == c && *(s + i) != '\0')
+		while (s[i] == c && s[i] != '\0')
 			i++;
-		if (*(s + i) != '\0')
+		if (s[i] != '\0')
 		{
 			count++;
 			i++;
 		}
-		while (*(s + i) != c && *(s + i) != '\0')
+		while (s[i] != c && s[i] != '\0')
 			i++;
 	}
 	return (count);
@@ -54,7 +54,7 @@ static int	ft_wordlen(char const *s, char c, int start)
 
 	i = start;
 	count = 0;
-	while (*(s + i) && (*(s + i) != c))
+	while (s[i] && s[i] != c)
 	{
 		count++;
 		i++;
@@ -72,15 +72,11 @@ static char	*ft_getword(char const *s, char c, int start)
 	i = start;
 	j = 0;
 	len = ft_wordlen(s, c, start);
-	word = (char *)malloc((len + 1) * sizeof(char));
+	word = (char *)malloc(sizeof(char) * (len + 1));
 	if (!word)
 		return (NULL);
-	while (*(s + i) != c && *(s + i) != '\0')
-	{
-		word[j] = *(s + i);
-		i++;
-		j++;
-	}
+	while (s[i] != c && s[i] != '\0')
+		word[j++] = s[i++];
 	word[j] = '\0';
 	return (word);
 }
@@ -92,194 +88,27 @@ char	**ft_split(char const *s, char c)
 	char	**arr;
 
 	i = 0;
-	j = 0;
+	j = -1;
 	if (!s)
 		return (NULL);
-	arr = (char **)malloc((ft_wordcount(s, c) + 1) * sizeof(char *));
+	arr = (char **)malloc((sizeof(char *) * (ft_wordcount(s, c) + 1)));
 	if (!arr)
 		return (NULL);
-	while (*(s + i) != '\0')
+	while (s[i])
 	{
-		while (*(s + i) == c && *(s + i) != '\0')
+		while (s[i] == c && s[i] != '\0')
 			i++;
-		if (*(s + i) != '\0')
-		{
-			arr[j++] = ft_getword(s, c, i++);
-		}
-		while (*(s + i) != c && *(s + i) != '\0')
+		if (s[i] != '\0')
+			arr[++j] = ft_getword(s, c, i++);
+		while (s[i] != c && s[i] != '\0')
 			i++;
 	}
-	arr[j] = NULL;
+	arr[++j] = NULL;
 	return (arr);
 }
 
-/* BUGGED
-static int	ft_wordcount(const char *s, char c)
+/*int	main(void)
 {
-	int	i;
-	int	count;
-
-	i = 0;
-	count = 0;
-	while (*(s + i) != '\0')
-	{
-		while ((*(s + i) == c) && (*(s + i) != '\0'))
-			i++;
-		if (*(s + i) != '\0')
-		{
-			count++;
-			i++;
-		}
-		while ((*(s + i) != c) && (*(s + i) != '\0'))
-			i++;
-	}
-	return (count);
-}
-
-static int	ft_wordlen(const char *s, char c, int start)
-{
-	int	i;
-
-	i = start;
-	while ((*(s + i)) && (*(s + i) != c))
-	{
-		i++;
-	}
-	return (i);
-}
-
-static char	*ft_getword(const char *s, char c, int start)
-{
-	char	*word;
-	int		len;
-	int		i;
-	int		j;
-
-	i = start;
-	j = 0;
-	len = ft_wordlen(s, c, start);
-	word = (char *)malloc((len + 1) * sizeof(char));
-	if (!word)
-		return (NULL);
-	while (*(s + i) == c)
-		i++;
-	while (*(s + i) != c && *(s + i) != '\0')
-	{
-		word[j] = *(s + i);
-		i++;
-		j++;
-	}
-	word[j] = '\0';
-	return (word);
-}
-
-char	**ft_split(char const *s, char c)
-{
-	int		i;
-	int		j;
-	char	**res;
-
-	if (s == NULL)
-		return (NULL);
-	i = 0;
-	j = 0;
-	res = (char **)malloc(sizeof(char *) * (ft_wordcount(s, c)) + 1);
-	if (!res)
-		return (NULL);
-	while (*(s + i) != '\0')
-	{
-		while (*(s + i) == c && *(s + i) != '\0')
-			i++;
-		if (*(s + i) != '\0')
-		{
-			res[j++] = ft_getword(s, c, i++);
-		}
-		while (*(s + i) != c && *(s + i) != '\0')
-			i++;
-	}
-	res[j] = NULL;
-	return (res);
-}
-
-------------------
-
-#include<stdio.h>
-static int	ft_wordlen(const char *s, char c, int index_start)
-{
-	int	start;
-
-	start = index_start;
-	while ((*(s + start) != '\0') && (*(s + start) != c))
-		start++;
-	return (start);
-}
-static int	ft_wordcounter(const char *s, char c)
-{
-	int	i;
-	int	count;
-
-	i = 0;
-	count = 0;
-	while (*(s + i) != '\0')
-	{
-		while ((*(s + i) != '\0') && (*(s + i) == c))
-			i++;
-		if (*(s + i) != '\0')
-		{
-			count++;
-			i++;
-		}
-		while ((*(s + i) != '\0') && (*(s + i) != c))
-			i++;
-	}
-	return (count);
-}
-char	**ft_split(const char *s, char c)
-{
-	char **res;
-	size_t	lenfind;
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	j = 0;
-	res = (char **)malloc(sizeof(char) * (ft_wordcounter(s, c) + 1));
-	if (!res)
-		return (NULL);
-	while (*(s + i) != '\0')
-	{
-		while ((*(s + i) != '\0') && (*(s + i) == c))
-			i++;
-		if (*(s + i) != '\0')
-		{
-			lenfind = (ft_wordlen(s, c, i));
-			res[j] = ft_substr(s, i, lenfind - i);
-			printf("Indice: %lu, lenfind: %lu\n", i, lenfind);
-			j++;
-			i++;
-		}
-		while ((*(s + i) != '\0') && (*(s + i) != c))
-			i++;
-	}
-	res[j] = NULL;
-	return (res);
-}
-
-#include"ft_substr.c"
-#include <stdio.h>
-int	main()
-{
-	int		index;
-	char	c = 'x';
-	char	*str = "xxcasaxvaca";;
-	char	**split;
-	split = ft_split(str, c);
-	index = 0;
-	while (split[index])
-	{
-		printf("%s\n", split[index]);
-		index++;
-	}
-	printf("%s\n", str);
-}
-*/
+	ft_split("var=kelvin", '=');
+	return 0;
+}*/
--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,31 +6,29 @@
 /*   By: kfaustin <kfaustin@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 13:15:45 by kfaustin          #+#    #+#             */
-/*   Updated: 2022/11/27 15:14:48 by kfaustin         ###   ########.fr       */
+/*   Updated: 2023/05/22 14:15:16 by kfaustin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
 
-/*
-**	The  ft_strncmp()  function  is similar, except it compares only the
-**	first (at most) n bytes of s1 and s2.
-**
-**	The ft_strcmp() and ft_strncmp() functions return an integer less than, equal
-**	to, or greater than zero if s1 (or the first n bytes thereof) is found,
-**	respectively, to be less than, to match, or be greater than s2.
-*/
-
+/**
+ * @brief The  ft_strncmp()  function  is similar, except it compares only the
+**		first (at most) n bytes of s1 and s2.
+ * @param s1	string s1
+ * @param s2	string s2
+ * @param n		n bytes to compare
+ * @return 	int
+ */
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
 	i = 0;
-	while (((*(s1 + i) != '\0') || (*(s2 + i) != '\0')) && (i < n))
+	while ((s1[i] || s2[i]) && (i < n))
 	{
-		if ((*(s1 + i)) != (*(s2 + i)))
-			return ((*(unsigned char *)(s1 + i))
-			- (*(unsigned char *)(s2 + i)));
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 		i++;
 	}
 	return (0);
